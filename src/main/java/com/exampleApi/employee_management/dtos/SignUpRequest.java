85f8314e92d210985ac01d7e26ad74a14e0c2b01
Abdelrahman Farag <>
2025-06-22 15:09:56 +0900

    no message

--- src/main/java/com/exampleApi/employee_management/dtos/SignUpRequest.java
+++ src/main/java/com/exampleApi/employee_management/dtos/SignUpRequest.java
@@ -3,7 +3,7 @@ package com.exampleApi.employee_management.dtos;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record SignupRequest(
+public record SignUpRequest(
         @NotNull(message = "username is required") @Size(min = 2, max = 50, message = "min is 2 characters and max is 50 characters") String username,
 
         @NotNull(message = "password is required") @Size(min = 2, max = 50, message = "min is 2 characters and max is 50 characters") String password) {
